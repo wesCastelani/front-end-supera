3b14eafe778bf5c155e52f0ef4c55cc155693555
wesley.castelani@hotmail.com <wesley.castelani@hotmail.com>
2022-12-20 20:54:50 -0300

    Form fazendo os requests certinhos (:

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,14 +1,45 @@
 import axios from "axios";
 
-const baseURL = 'http://localhost:8080/transferencias/'
+const baseURL = "http://localhost:8080/transferencias/";
 
-
-
-async function getTrasacoesPorConta(id){
-    const res = await axios.get(baseURL + id)
-    const data = res.data
-    return data
+async function getTrasacoesPorConta(id) {
+  const res = await axios.get(baseURL + id);
+  const data = res.data;
+  return data;
 }
 
+async function filtrarTransacoes(id, Object) {
+  const filtro = {
+    inicio: Object.inicio,
+    fim: Object.fim,
+    nomeOperador: Object.nomeOperador,
+  };
 
-export {getTrasacoesPorConta}
+  if (filtro.inicio == "" && filtro.fim == "" && filtro.nomeOperador == "") {
+    const res = await axios.get(baseURL + id);
+    const data = res.data;
+    return data;
+  } else if (
+    filtro.inicio == "" &&
+    filtro.fim == "" &&
+    filtro.nomeOperador != ""
+  ) {
+    const res = await axios.post(baseURL + id + "/operador", filtro);
+    const data = res.data;
+    return data;
+  } else if (
+    filtro.inicio != "" &&
+    filtro.fim != "" &&
+    filtro.nomeOperador == ""
+  ) {
+    const res = await axios.post(baseURL + id + "/periodo", filtro);
+    const data = res.data;
+    return data;
+  } else {
+    const res = await axios.post(baseURL + id + "/periodo/operador", filtro);
+    const data = res.data;
+    return data;
+  }
+}
+
+export { getTrasacoesPorConta, filtrarTransacoes };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,69 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-import * as api from '../api/apiService'
+import * as api from "../api/apiService";
 
 const Home = () => {
-
   const params = useParams();
 
-  const [transacoes, setTrasacoes] = useState([])
-  const [saldoTotal, setSaldoTotal] = useState([])
+  const [transacoes, setTrasacoes] = useState([]);
+  const [saldoTotal, setSaldoTotal] = useState([]);
+  const [saldoTotalFiltrado, setSaldoTotalFiltrado] = useState([]);
 
   useEffect(() => {
     const getTransactions = async () => {
       let t = await api.getTrasacoesPorConta(params.id);
-      setTimeout(()=>{
-        setTrasacoes(t.transferencias)
-        setSaldoTotal(t.saldo)
-      }, 1000)
-      console.log(saldoTotal)
+      setTrasacoes(t.transferencias);
+      setSaldoTotal(t.saldo);
     };
     getTransactions();
   }, [saldoTotal]);
 
+  const formSubmit = (event) => {
+    event.preventDefault();
+    var data = new FormData(event.target);
+    let formObject = Object.fromEntries(data.entries());
+    console.log(formObject);
+    const getTransactions = async () => {
+      let t = await api.filtrarTransacoes(params.id, formObject);
+      setTrasacoes(t.transferencias);
+      setSaldoTotalFiltrado(t.saldo);
+      console.log(transacoes);
+    };
+    getTransactions();
+  };
 
   return (
-    <div className="App">
-      <h2>Seu saldo é: {saldoTotal}</h2>
+    <div className="container">
+      <form
+        onSubmit={formSubmit}
+        className="d-flex flex-row align-items-center flex-wrap mt-5"
+      >
+        <label className="my-1 me-2">Data Inicio</label>
+        <input
+          type="date"
+          name="inicio"
+          className="form-control my-1 me-sm-2 w-auto"
+        />
+        <label className="my-1 me-2">Data fim</label>
+        <input
+          type="date"
+          name="fim"
+          className="form-control my-1 me-sm-2 w-auto"
+        />
+        <label className="my-1 me-2">Nome do operador</label>
+        <input
+          type="text"
+          name="nomeOperador"
+          className="form-control my-1 me-sm-2 w-auto"
+        />
+
+        <button type="submit" className="btn btn-primary my-1">
+          Filtrar
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default Home;
